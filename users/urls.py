3742a2f3af05d74fea0131d56348664da2411f59
gh0st <maratka2202@gmail.com>
2024-01-29 20:12:40 +0300

    Login system bugfixes (django5 update)

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,15 @@
 from typing import List
 
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as av
 
-from users.views import user_register
+from .views import user_register
 
 urlpatterns: List = [
-    path("login/", auth_views.LoginView.as_view(
+    path("login/", av.LoginView.as_view(
         template_name="user_login.html"
     ), name="user_login"),
-    path("logout/", auth_views.LogoutView.as_view(
+    path("logout/", av.LogoutView.as_view(
         template_name="user_logout.html"
     ), name="user_logout"),
     path("register/", user_register, name="user_register"),
